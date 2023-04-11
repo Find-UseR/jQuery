d39e0674d91f5b493c5b84ffd8329620adcfd843
AQiB <122538985+Find-UseR@users.noreply.github.com>
2023-04-11 18:12:36 +0500

    Add files via upload

---
+++ 07 Class - Slider Button/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.slider-part').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<i class="fas fa-chevron-left prev-arr"></i>',
+        nextArrow: '<i class="fas fa-chevron-right nxt-arr"></i>',
+    });
+});
