2c605bd2022e2c89114899e2457ecded3f6e7485
AQiB <122538985+Find-UseR@users.noreply.github.com>
2023-04-11 18:28:20 +0500

    Add files via upload

---
+++ 08 class - Typed/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+   
+    $(".typed").typed({
+        strings: [" Hello ", " My Name is Aqib ", " I'm a Web Developer "],
+        loop: true,
+        typeSpeed: 30,
+        startDelay: 1200,
+        backSpeed: 20,
+        backDelay: 500,
+        loopCount: 500,
+        showCursor: false,
+    })
+    
+});
